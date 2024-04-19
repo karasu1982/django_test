504144432191ca52ecd3f651b8e5da26e0799a80
masahiko.karasudani <masahiko.karasudani@gmail.com>
2024-04-19 19:21:32 +0900

    Add models.h5

--- crud/views.py
+++ crud/views.py
@@ -1,20 +1,45 @@
 from django.shortcuts import render
 from .forms import ImageUploadForm
-import random
+# import random
 
+from django.conf import settings
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from io import BytesIO
+import os
+
+
+# Create your views here.
 def predict(request):
-    if request.method == 'GET':
-        form = ImageUploadForm()
-        return render(request, 'home.html', {'form': form})
-        
-    if request.method == 'POST':
-        form = ImageUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            img_file = form.cleaned_data['image']
-            prediction = random.choice(["猫", "犬"])
+  if request.method == 'GET':
+    # GETリクエストによるアクセス時の処理を記述
+    form = ImageUploadForm()
+    return render(request, 'home.html', {'form': form})  
 
-            return render(request, 'home.html', {'form': form, 'prediction': prediction})
+  if request.method == 'POST':
+    # POSTリクエストによるアクセス時の処理を記述
+    form = ImageUploadForm(request.POST, request.FILES)
 
-        else:
-            form = ImageUploadForm()
-            return render(request, 'home.html', {'form': form})
+    if form.is_valid():
+      img_file = form.cleaned_data['image']
+      img_file = BytesIO(img_file.read())
+      img = load_img(img_file, target_size=(256, 256))
+      img_array = img_to_array(img)
+      img_array = img_array.reshape((1, 256, 256, 3))
+      img_array = img_array/255
+
+      model_path = os.path.join(settings.BASE_DIR, 'prediction', 'models', 'model.h5')
+      model = load_model(model_path)
+      result = model.predict(img_array)
+      if result[0][0] > result[0][1]:
+        prediction = '猫'
+      else:
+        prediction = '犬'
+
+#      prediction = random.choice(["猫", "犬"])
+      return render(request, 'home.html', {'form': form, 'prediction': prediction})
+
+    else:
+      form = ImageUploadForm()
+      return render(request, 'home.html', {'form': form})
